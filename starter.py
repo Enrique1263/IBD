3e76566e74639df5cb910882d16df4d5729edd03
bikuta6 <151018166+bikuta6@users.noreply.github.com>
2024-03-15 18:34:15 +0100

    First push
    Docker compose que levanta el replicaset de mongo, un script de python dentro de un contenedor de imagen descrita por Dockerfile-1 y por si se quieren probar cosas, un contenedor jupyter sin contraseñas para comprobar que se están insertando bien los datos y que los nodos están funcionales

---
+++ starter.py
@@ -0,0 +1,30 @@
+import pymongo
+
+# MongoDB connection string
+connection_string = 'mongodb://mongo1:27017,mongo2:27018,mongo3:27019/?replicaSet=rs0'
+
+# Connecting to MongoDB
+client = pymongo.MongoClient(connection_string)
+
+# Database name
+db_name = 'news'
+
+# Check if the database exists
+dblist = client.list_database_names()
+if db_name not in dblist:
+    print(f"Database '{db_name}' does not exist. Creating...")
+    
+    # Creating the database by creating collections since MongoDB creates databases and collections automatically when you first store data in them
+    db = client[db_name]
+    
+    # Collections to be created
+    collections = ['politics', 'entertainment', 'economy', 'technology']
+    
+    for collection in collections:
+        db.create_collection(collection)
+        print(f"Collection '{collection}' created.")
+else:
+    print(f"Database '{db_name}' already exists.")
+
+# Closing the connection
+client.close()
